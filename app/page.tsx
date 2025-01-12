4d6b353ee9128503f097bb909ba2ae86f5ec9725
alexseva94 <alexseva94@gmail.com>
2025-01-12 13:43:22 +0200

    fix-build

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Image
         className="dark:invert"
-        src="sag-way/next.svg"
+        src="/next.svg"
         alt="Next.js logo"
         width={180}
         height={38}
