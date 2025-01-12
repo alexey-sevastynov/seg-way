8884bf73652212f2d4299d1d6174ef497501a5b5
FernirTeam <silentoven@fernir.co>
2025-01-12 12:42:39 +0200

    deploy-to-github-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/sag-way",
+  assetPrefix: "/sag-way",
+  output: "export",
 };
 
 export default nextConfig;
