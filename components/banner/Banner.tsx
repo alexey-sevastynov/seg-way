ee0cab7d64076f0f1219b1a439a0e1c15dd1974f
Dmytro Shevchenko <0911dimon@gmail.com>
2025-02-25 20:38:39 +0200

    16-create-banner-component

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -6,7 +6,7 @@ interface BannerProps {
 export function Banner({ text }: BannerProps) {
     return (
         <div className="flex h-12 items-center justify-center bg-blue text-white max-md:bg-inherit max-md:text-black">
-            <p className="uppercase">{text}</p>
+            <p className="md:uppercase">{text}</p>
         </div>
     );
 }
